783ff8710b515cf8701855ef449aa37b2e6539d0
diaba <hwar@cincinnatistate.edu>
2022-01-14 22:47:07 -0500

    Style the website

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
+const hamburgerMenu = document.querySelector('#navigation .nav-icon');
+const navContent = document.querySelector('#nav-content');
+const closeNavButton = document.querySelector('#nav-content .close-btn');
+const navLinks = document.querySelectorAll('#nav-content nav ul li a');
+const scrollButton = document.querySelector(".scroll-top");
+
+// Open the menu
+document.querySelector('.first-button').addEventListener('click', function () {
+  // alert('click')
+   document.querySelector('.animated-icon1').classList.toggle('open');
+   });
+
+// scroll TOP Button Events
+
+if(scrollButton){
+  window.addEventListener('scroll', ()=> {
+    if(pageYOffset > (window.innerHeight * 1.2)){
+      scrollButton.style.display="flex";
+    }else{
+      scrollButton.style.display="none";
+    }
+  });
+  scrollButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+}
+
+// Hamburger Menu events
+hamburgerMenu.addEventListener('click', ()=>{
+  navContent.classList.add('show');
+  document.body.style.overflow="hidden";
+});
+closeNavButton.addEventListener('click', ()=>{
+  navContent.classList.remove('show');
+  document.body.style.overflow="initial";
+});
+navLinks.forEach( link => {
+  link.addEventListener('click', ()=> {
+    navContent.classList.remove('show');
+    document.body.style.overflow="initial";
+  })
+})
+
+
+// 
+// 
+// 
+
 const idImg = document.querySelector("#slide");
 for (var i = 2; i< 30; i++){
    const image = document.createElement('img')
    image.src  = `https://diaba.github.io/portfolio/images/Slide${i}.jpeg`
    idImg.appendChild(image)
 }
+
+////
